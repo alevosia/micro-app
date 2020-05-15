4ba066eb1220d5fe12fc2c24bb840f554e50c010
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-05-15 18:27:50 +0800

    Add version tags on dependencies

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export { Micro } from 'https://raw.githubusercontent.com/alevosia/micro/master/Micro.ts'
 
 // fs
-export { readFileStr } from 'https://deno.land/std/fs/read_file_str.ts';
-export { exists } from 'https://deno.land/std/fs/exists.ts';
+export { readFileStr } from 'https://deno.land/std@v0.51.0/fs/read_file_str.ts';
+export { exists } from 'https://deno.land/std@v0.51.0/fs/exists.ts';
 
 // path
-export * as path from 'https://deno.land/std/path/mod.ts';
+export * as path from 'https://deno.land/std@v0.51.0/path/mod.ts';
