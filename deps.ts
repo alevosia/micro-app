b90999efc5ec11eb45f3c5a547ec65c31f2eed27
Alexander Paul <alexanderpaul.marinas@gmail.com>
2020-05-15 17:56:21 +0800

    Add more routes and static route

--- deps.ts
+++ deps.ts
@@ -1,1 +1,8 @@
-export { Micro } from 'https://raw.githubusercontent.com/alevosia/micro/master/structures/Micro.ts'
+export { Micro } from 'https://raw.githubusercontent.com/alevosia/micro/master/Micro.ts'
+
+// fs
+export { readFileStr } from 'https://deno.land/std/fs/read_file_str.ts';
+export { exists } from 'https://deno.land/std/fs/exists.ts';
+
+// path
+export * as path from 'https://deno.land/std/path/mod.ts';
--- server.ts
+++ server.ts
@@ -1,13 +1,30 @@
-const PORT = 3000
+const PORT = 5000
 
-import { Micro } from './deps.ts'
+import { Micro, path, readFileStr } from './deps.ts'
 
 const app = new Micro()
 
-app.get('/', (req) => {
-    req.respond({ body: 'Hello World!'})
+// add logger middleware
+app.use((request) => {
+    console.log(`${request.method} ${request.url}`)
+})
+
+app.static('/static', path.join(Deno.cwd(), 'public'))
+
+app.get('/', async (request) => {
+    const filePath = path.join(Deno.cwd(), 'public', 'index.html')
+    const file = await readFileStr(filePath)
+    return request.respond({ body: file })
+})
+
+app.get('/auth', (request) => {
+    request.respond({ body: 'Auth Page'})
+})
+
+app.post('/users', (request) => {
+    request.respond({ body: 'Submit User Data'})
 })
 
 app.listen(PORT, () => {
-    console.log(`App is now listening port ${PORT}`)
+    console.log(`Server is now listening to port ${PORT}`)
 })
